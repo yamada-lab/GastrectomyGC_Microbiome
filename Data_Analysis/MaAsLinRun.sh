c4c3836c29decfcc117e1400ca8ebac14ff6eb0a
pande <erawijantari.p.aa@m.titech.ac.jp>
2019-11-14 11:59:58 +0900

    Update MaAsLinRun.sh

--- Data_Analysis/MaAsLinRun.sh
+++ Data_Analysis/MaAsLinRun.sh
@@ -23,7 +23,7 @@ adjustfile=$6
 
 step1(){
 	#Data preparations
-time python ~/gut_microbiome_analysis/script/ReRunOrganized/StastViz/MaAsLIn_formated.py $relab $metadata $title $referenceID
+time python ./Data_Analysis/MaAsLIn_formated.py $relab $metadata $title $referenceID
 time python $MaAsLin/exec/merge_metadata.py $title.Metadata.tsv < $relab > $relab.MaAsLin.pcl
 time python $MaAsin/exec/transpose.py < $relab.MaAsLin.pcl >  $relab.MaAsLin.tsv
 }
@@ -40,8 +40,8 @@ step3(){
 #adjusted
 mkdir MaAsLin_Adjusted/confound_factor
 while read p; do
-		time python ~/gut_microbiome_analysis/script/ReRunOrganized/StastViz/ConfoundVarExp.py ${dir}/${title}_All/${relab}-Status.txt ${dir}/${title}_adj.${p}/${relab}-Status.txt confound_factor/deconfound.${p}.pdf -r $relab -s $statscombine
-		time python ~/gut_microbiome_analysis/script/ReRunOrganized/StastViz/ConfoundVarExp.py ${dir}/${title}_All/${relab}-Status.txt ${dir}/${title}_All/${relab}-${BMI}.txt confound_factor/${p}.pdf -r $relab -s $statscombine -a "Yes"
+		time python ./Data_Analysis/ConfoundVarExp.py ${dir}/${title}_All/${relab}-Status.txt ${dir}/${title}_adj.${p}/${relab}-Status.txt confound_factor/deconfound.${p}.pdf -r $relab -s $statscombine
+		time python ./Data_Analysis/ConfoundVarExp.py ${dir}/${title}_All/${relab}-Status.txt ${dir}/${title}_All/${relab}-${BMI}.txt confound_factor/${p}.pdf -r $relab -s $statscombine -a "Yes"
 	done < $adjustfile
 }
 
@@ -51,19 +51,6 @@ step2
 step3
 
 
-#example
-#MetaPhlan2
-#bash ~/gut_microbiome_analysis/script/ReRunOrganized/StastViz/MaAsLinRun.sh 2018-12-07_Metaphlan2_Species.5.0.001.up.tsv ~/gut_microbiome_analysis/gastrectomy_early/181103_2Group/181109_Metadata/MetaDisBlood/190822_MaAsLinMetadata.tsv 190822MetaPhlan2 ~/gut_microbiome_analysis/gastrectomy_early/list_data/2018-10-21_sampleID_seqIDNoEMR ../190111_Stats_Combine_SpeciesMetaphlan2.tsv 
-
-#mOTU
-#bash ~/gut_microbiome_analysis/script/ReRunOrganized/StastViz/MaAsLinRun.sh 2018-11-03_NCBI.species.abundances.5.1e-05.up.tsv ~/gut_microbiome_analysis/gastrectomy_early/181103_2Group/181109_Metadata/MetaDisBlood/190822_MaAsLinMetadata.tsv 190822mOTU ~/gut_microbiome_analysis/gastrectomy_early/list_data/2018-10-21_sampleID_seqIDNoEMR ../190111_Stats_Combine_SpeciesNCBI.tsv
-
-#Humann2
-#bash ~/gut_microbiome_analysis/script/ReRunOrganized/StastViz/MaAsLinRun.sh 181218_Modules.Specifiy.5.1e-06.up.tsv ~/gut_microbiome_analysis/gastrectomy_early/181103_2Group/181109_Metadata/MetaDisBlood/190822_MaAsLinMetadata.tsv 190822Humann2 ~/gut_microbiome_analysis/gastrectomy_early/list_data/2018-10-21_sampleID_seqIDNoEMR ../190111_Stats_Combine_ModuleUniref.tsv
-
-
-#KEGG
-#bash ~/gut_microbiome_analysis/script/ReRunOrganized/StastViz/MaAsLinRun.sh 190117_KEGG_modules.5.1e-06.up.tsv ~/gut_microbiome_analysis/gastrectomy_early/181103_2Group/181109_Metadata/MetaDisBlood/190822_MaAsLinMetadata.tsv 190822KEGG ~/gut_microbiome_analysis/gastrectomy_early/list_data/2018-10-21_sampleID_seqIDNoEMR ../190111_Stats_Combine_ModuleKEGG.tsv
 
 #Metabolome
 #bash ~/gut_microbiome_analysis/script/ReRunOrganized/StastViz/MaAsLinRun.sh 190402_Metabolome.4.1e-06.upEdited.tsv ~/gut_microbiome_analysis/gastrectomy_early/181103_2Group/181109_Metadata/MetaDisBlood/190822_MaAsLinMetadata.tsv 190822Metabolome ~/gut_microbiome_analysis/gastrectomy_early/list_data/2018-10-21_sampleID_seqIDNoEMR ../190402_StatsCombineMetabolome.tsv
